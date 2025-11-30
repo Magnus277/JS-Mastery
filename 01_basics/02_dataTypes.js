11bea63be544e0784b5a78669ba711a25101843a
Debayan <ddebayan29@gmail.com>
2025-11-30 14:12:16 +0530

    add initial data types example in 02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3); // we are using nodejs, not browser
+
+console.log(3 + 3);
+
+
+let name = "debayan"
+let age = 18
+let isLoggedIn = false
+let state = null // null is khali
+
+//number => 2 to power 53
+//bigint
+// string => ""
+//boolean => true/false
+// null => standalone value
+// undefined => 
+  // symbol => unique
+
+
+
+
+
+// object 
+
+console.log(typeof "debayan")
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
