c64f4c10db953093a4da62428358659456598ed7
Debayan <ddebayan29@gmail.com>
2025-11-30 14:29:37 +0530

    refactor conversion operations in 03_conversionOperation.js for clarity

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,16 +1,13 @@
 // let score = "33abc"
 // let score = null
 // let score = undefined
-let score = true
+let score = true;
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
-
-
-
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -18,17 +15,47 @@ console.log(valueInNumber);
 
 // let isLoggedIn = 1
 // let isLoggedIn = ""
-let isLoggedIn = "deb"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
-
+let isLoggedIn = "deb";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
 
 // 1=>true; 0 => false
 // "" => false
 // "deb" => true
 
-let someNumber = 33
+let someNumber = 33;
 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************** Operations ***************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = "hitesh";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
