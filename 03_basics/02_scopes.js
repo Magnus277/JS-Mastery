0e14703d065648892bf04170d977617986100a31
Debayan <ddebayan29@gmail.com>
2025-12-01 22:18:36 +0530

    refactor 02_scopes.js and add tempCodeRunnerFile.js to improve code organization and readability

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,14 +1,59 @@
-let a = 300
+// let a = 300
 
-if (true) {
-  let a = 10;
-  const b = 20;
-  console.log("inner: ", a);
+// if (true) {
+//   let a = 10;
+//   const b = 20;
+//   console.log("Inner: ", a);
   
+// }
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c); //var doesnt understand scopes
+
+
+function one() {
+  const username = "debayan"
+
+  function two() {
+      const website = "youtube"
+      console.log(username);
+  }
+  // console.log(website);
+
+  two()
+  
+}
+
+// one()
+
+if(true) {
+  const username = "debayan"
+  if(username == "debayan") {
+    const website = " youtube"
+    // console.log(username + website);
+    
+  }
+  // console.log(website);
+  
+}
+
+// console.log(username);
+
+
+// *************************** Interesting !!! *********************************
+
+console.log(addOne(5))
+function addOne(num) {
+    return num + 1
 }
 
 
 
-console.log(a);
-// console.log(b);
-// console.log(c); //var doesnt understand scopes
+// console.log(addTwo(5)) // error
+const addTwo = function(num) {
+    return num + 2
+}
+
