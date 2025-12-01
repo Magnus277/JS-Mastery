e7281c169671d419ad132d728285b0f0d100ccff
Debayan <ddebayan29@gmail.com>
2025-12-01 16:08:43 +0530

    add 07_dates.js and tempCodeRunnerFile.js with examples for date operations

---
+++ 01_basics/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON());
+// console.log(typeof myDate); //object
