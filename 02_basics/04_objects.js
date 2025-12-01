ba8585363a40c91cfad574c9c1457bb208b2bbed
Debayan <ddebayan29@gmail.com>
2025-12-01 21:15:48 +0530

    add 04_objects.js with examples of object creation and manipulation

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,57 @@
+// const tinderUser = new Object() //singleton object
+const tinderUser = {} //non singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name= "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "debayan",
+            lastname: "das"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.lastname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+
+// const obj3 = { obj1,obj2 }
+
+// const obj3 = Object.assign({}, obj1, obj2, obj4) //{} is target here and obj123 is source
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const users = [
+  {
+    id:1,
+    email: "h@gmail.com"
+  },
+  {
+    id:1,
+    email: "h@gmail.com"
+  },
+  {
+    id:1,
+    email: "h@gmail.com"
+  },
+]
+
+
+// console.log(users[1].email)
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser)); //returns a array of keys
+console.log(Object.values(tinderUser)); //returns a array of values
+console.log(Object.entries(tinderUser)); //returns a array of arrays
+
+console.log(tinderUser.hasOwnProperty('isLoggedI'));
