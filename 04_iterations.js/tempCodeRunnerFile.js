5f760d38b0b8f2c75be595e562992d110506e27c
Debayan <ddebayan29@gmail.com>
2025-12-02 13:49:17 +0530

    add examples of reduce method for array summation and filter method for object properties

--- 04_iterations.js/tempCodeRunnerFile.js
+++ 04_iterations.js/tempCodeRunnerFile.js
@@ -1,9 +1,7 @@
-const map = new Map()
-map.set('IN', "India")
-map.set('USA', "United States of America")
-map.set('Fr', "France")
-map.set('IN', "India")
+const coding = ["js", "ruby", "java", "python", "cpp"]
 
-for (const key in map) {
-    console.log(key);
-}
+
+const values = coding.forEach( (item) => {
+    //console.log(item);
+    return item
+} )
