d9ed69a537013cea41f6615d4ddf1e9a40ee35d9
Debayan <ddebayan29@gmail.com>
2025-12-07 20:49:58 +0530

    Object Properties

---
+++ 10_oops/mathpi.js
@@ -0,0 +1,32 @@
+const descriptor = Object.getOwnPropertyDescriptor(Math, 'PI')
+
+console.log(descriptor);
+
+
+// console.log(Math.PI);
+
+
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log("Chai nehi bani");
+        
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, 'name', {
+  // writable: false,
+  enumerable: false, // name property wont be printed
+})
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+  if(typeof value !== 'function')
+    console.log(`${key} : ${value}`);
+    
+}
